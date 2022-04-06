1193f76bef0aa984ac52f049b5e58d6de29641d7
BIBABOBASHULMAN <dan1360101@gmail.com>
2022-04-06 11:42:33 +0300

    inition1

--- practos 3.1.py
+++ practos 3.1.py
@@ -1,4 +1,4 @@
-name = 'Daniil'
-age = '18'
-city = 'Minsk'
-print("Menya zovut {}. Mne {} let. Ya iz {}.".format(name, age, city)
+name = input('Vvodim imya: ')
+age = input('Vvodim vozrast: ')
+city = input('Vvodim gorod: ')
+print("Menya zovut {}. Mne {} let. Ya iz {}.".format(name, age, city))
